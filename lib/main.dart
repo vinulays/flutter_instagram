46ddefc29439dba099987ee65751f81fe504d7da
Vinula Senarathne <vinulapc@gmail.com>
2023-11-30 11:27:07 +0530

    feat: login ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram/responsive/mobile_screen_layout.dart';
 import 'package:flutter_instagram/responsive/responsive_screen_layout.dart';
 import 'package:flutter_instagram/responsive/web_screen_layout.dart';
+import 'package:flutter_instagram/screens/login_screen.dart';
 import 'package:flutter_instagram/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyDTMXVM-bXqAJXUmymIcaQZXfJKJLb49rM",
+        appId: "1:476911956705:web:9b0f083d9e32e2b077bda0",
+        messagingSenderId: "476911956705",
+        projectId: "flutter-instagram-57608",
+        storageBucket: "flutter-instagram-57608.appspot.com"),
+  );
+
   runApp(const MyApp());
 }
 
@@ -19,9 +33,10 @@ class MyApp extends StatelessWidget {
       title: 'Instagram',
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: const ResponsiveLayout(
-          mobileScreenLayout: MobileScreenLayout(),
-          webScreenLayout: WebScreenLayout()),
+      // home: const ResponsiveLayout(
+      //     mobileScreenLayout: MobileScreenLayout(),
+      //     webScreenLayout: WebScreenLayout()),
+      home: const LoginScreen(),
     );
   }
 }
